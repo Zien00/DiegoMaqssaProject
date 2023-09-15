a75398c50b5f4acbb4312fa721ebd085a334caed
Diego de Sousa <diemaq19@gmail.com>
2023-09-15 19:42:49 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMode() {
+const html = document.documentElement
+
+ if(html.classList.contains('light')) {
+   html.classList.remove('light')
+  }else{
+    html.classList.add('light')
+  }
+
+  const img = document.querySelector("#profile img")
+
+
+
+  if(html.classList.contains('light')) {
+img.setAttribute('src','./assets/assets/my-avatar-light.jpg')
+
+
+
+  }else {
+
+img.setAttribute("src", "./assets/assets/my-avatar.jpg")
+
+  }
+}
